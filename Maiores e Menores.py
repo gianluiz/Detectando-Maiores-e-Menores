35e7c6484e54af86e5441f9f8127b2d62766d36e
Gian Luiz <gianluizoliveira13@gmail.com>
2022-10-14 15:07:36 -0300

    #O programa que lê nome, idade e sexo de 8 pessoas. #No final mostra: #->a média de idade do grupo #->qual é o nome do homem mais velho #->quantas mulheres têm menos de 20 anos

---
+++ Maiores e Menores.py
@@ -0,0 +1,42 @@
+#O programa que lê nome, idade e sexo de 8 pessoas.
+#No final mostra:
+#->a média de idade do grupo
+#->qual é o nome do homem mais velho
+#->quantas mulheres têm menos de 20 anos
+cm = 0
+cont = 0
+mi = 0
+hvn = ""
+hvi = 0
+for c in range (1,9):
+    print(f"===Pessoa #{c}===")
+    while True:
+        nome = str(input("Nome:"))
+        if nome.isalpha():
+            break
+        elif nome.isnumeric():
+            print("ERRO.Digite apenas LETRAS e não NÚMEROS.")
+    while True:
+        try:
+            idade = int(input("Idade:"))
+            break
+        except ValueError:
+            print("ERRO!Digite um número!")
+    sexo = " "
+    while sexo not in "MF":
+        sexo = str(input("SEXO: [M] ou [F]?\nDIGITE apenas uma LETRA->")).strip().upper()
+    print("CADASTRADO(A)")
+    mi += idade
+    cont += 1
+    if sexo == "M":
+        if c == 1:
+            hvi = idade
+            hvn = nome
+        elif idade > hvi:
+            hvi = idade
+            hvn = nome
+    if sexo == "F" and idade < 20:
+        cm+= 1
+print(f"A média de idade do grupo é {mi/cont}")
+print(f"O nome do homem mais velho é {hvn}")
+print(f"E existem {cm} mulheres com menos de 20 anos.")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Detectando maiores e menores
- Um exercício simples com um raciocínio importante para vida do programador
+# Detectando Maiores e Menores (Este é um simples exercício que lê os dados de algumas pessoas e aponta os maiores ou menores dados.)
+
+->A resolução é rudimentar, mas eu fiz questão de fazer o registro deste raciocínio pois é e foi essencial nos meus estudos.
+Raciocínio lógico e primordial, simples, principalmente pelo fato das variáveis poderem, ou não, receber novos valores a cada loop. 
+->Mas isso faz toda a diferença e é muito utilizado com listas e dicionários.
+(Contém tratamentos de erros caso o usuário digite algo inesperado)  
+(Resolução rudimentar, ainda sem listas e outras funções)
